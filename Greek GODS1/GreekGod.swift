4e1bfb438c78029e451c61f5d70d2dc77df4be24
Sherry1481 <shareyscorpi1@gmail.com>
2021-07-27 14:30:51 +0500

    commit

---
+++ Greek GODS1/GreekGod.swift
@@ -0,0 +1,24 @@
+//
+//  GreekGod.swift
+//  Greek GODS1
+//
+//  Created by sherry on 27/07/2021.
+//
+
+import Foundation
+
+class GreekGod {
+    var name : String
+    var height : Double
+    var superPower: String
+    var internalId: Int
+    init( _name: String, _height: Double, _superPower: String, _internalId: Int)
+    {
+        name = _name
+        height = _height
+        superPower = _superPower
+        internalId = _internalId
+        
+    }
+}
+
