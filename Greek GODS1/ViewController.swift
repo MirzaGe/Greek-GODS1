9d27dc7f34fd424f56e27cbc3937c3ca240c25a8
Sherry1481 <shareyscorpi1@gmail.com>
2021-07-27 13:43:30 +0500

    Initial Commit

---
+++ Greek GODS1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Greek GODS1
+//
+//  Created by sherry on 27/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
